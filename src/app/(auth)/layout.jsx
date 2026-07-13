e39da8422b421ee42fa685893adbea65ec069ffc
seoyoon-k <its.yoon.work@gmail.com>
2026-07-13 13:47:49 +0900

    fix: mainlayout 상단 여백 변경

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex min-h-dvh items-center justify-center px-[15px]">
+    <main className="flex min-h-dvh items-center justify-center px-[15px]">
       <div className="w-full max-w-[345px] tablet:max-w-[440px] pc:max-w-[520px]">
         {children}
       </div>
-    </div>
-  )
+    </main>
+  );
 }
--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <div className="flex min-h-dvh flex-col">
       {/* Header*/}
-      <main className="flex-1 pt-[140px]">
+      <main className="flex-1 pt-[60px] tablet:pt-[70px] pc:pt-[80px]">
         <div className="mx-auto w-full max-w-[1920px] px-[15px] tablet:px-5 pc:px-[220px]">
           {children}
         </div>
