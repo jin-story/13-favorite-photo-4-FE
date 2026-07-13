f0d9ce63b202f9d9565a0d92737a9485722cc0af
apple <apple@appleui-MacBookAir.local>
2026-07-13 09:32:39 +0900

    chore: home에 있던 테스트 코드 삭제

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,10 +2,5 @@ import Chip from "@/components/Chip";
 import React from "react";
 
 export default function Home() {
-  return (
-    <div>
-      Home
-      <Chip />
-    </div>
-  );
+  return <div>Home</div>;
 }
