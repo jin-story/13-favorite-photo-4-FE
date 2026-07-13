a85acd29a44dbef15479a67c0e5f20954db9d41e
cari <aa34500388@gmail.com>
2026-07-13 16:20:59 +0900

    fix: css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,11 @@
+import GradeMyCard from "@/components/GradeMyCard";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <GradeMyCard grade="COMMON" count={20} size="L" />
+    </div>
+  );
 }
--- src/components/GradeMyCard.jsx
+++ src/components/GradeMyCard.jsx
@@ -1,23 +1,23 @@
 import clsx from "clsx";
 
 const gradeStyle = {
-  COMMON: "border-[#EFFF04] text-[#EFFF04]",
-  RARE: "border-[#29C9F9] text-[#29C9F9]",
-  SUPER_RARE: "border-[#A77EFF] text-[#A77EFF]",
-  LEGENDARY: "border-[#FF2A6A] text-[#FF2A6A]",
+  COMMON: "border-main text-main",
+  RARE: "border-blue text-blue",
+  SUPER_RARE: "border-purple text-purple",
+  LEGENDARY: "border-pink text-pink",
 };
 
 const sizeStyle = {
-  L: "h-[40px] text-[16px] px-[20px] py-[8px] gap-[10px]",
-  M: "h-[32px] text-[14px] px-[10px] py-[6px] gap-[10px]",
-  S: "h-[30px] text-[12px] px-[10px] py-[6px] gap-[10px]",
+  L: "h-[40px] text-[16px] px-[20px] py-[8px]",
+  M: "h-[32px] text-[14px] px-[10px] py-[6px]",
+  S: "h-[30px] text-[12px] px-[10px] py-[6px]",
 };
 
 export default function GradeMyCard({ grade, count = 0, size }) {
   return (
     <div
       className={clsx(
-        "inline-flex items-center border font-medium bg-[#0F0F0F]",
+        "inline-flex items-center border font-medium bg-black gap-[10px]",
         gradeStyle[grade],
         sizeStyle[size],
       )}
