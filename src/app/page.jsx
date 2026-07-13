fc31b128597d260218b8ff2406fe39a87b5cd831
cari <aa34500388@gmail.com>
2026-07-13 16:28:42 +0900

    fix: page.jsx 기본으로 되돌림

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,5 @@
-import GradeMyCard from "@/components/GradeMyCard";
 import React from "react";
 
 export default function Home() {
-  return (
-    <div>
-      Home
-      <GradeMyCard grade="COMMON" count={20} size="L" />
-    </div>
-  );
+  return <div> Home</div>;
 }
