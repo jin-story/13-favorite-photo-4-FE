b9ffb71f9d2f27eefef9616e8da25a7995bc9e8a
cari <aa34500388@gmail.com>
2026-07-13 15:48:05 +0900

    feat: GradeMyCard

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
+import Mycard from "@/components/Grade.js";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <div>Home</div>;
 }
---
+++ src/components/GradeMyCard.jsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+
+const gradeStyle = {
+  COMMON: "border-[#EFFF04] text-[#EFFF04]",
+  RARE: "border-[#29C9F9] text-[#29C9F9]",
+  SUPER_RARE: "border-[#A77EFF] text-[#A77EFF]",
+  LEGENDARY: "border-[#FF2A6A] text-[#FF2A6A]",
+};
+
+const sizeStyle = {
+  L: "h-[40px] text-[16px] px-[20px] py-[8px] gap-[10px]",
+  M: "h-[32px] text-[14px] px-[10px] py-[6px] gap-[10px]",
+  S: "h-[30px] text-[12px] px-[10px] py-[6px] gap-[10px]",
+};
+
+export default function GradeMyCard({ grade, count = 0, size }) {
+  return (
+    <div
+      className={clsx(
+        "inline-flex items-center border font-medium bg-[#0F0F0F]",
+        gradeStyle[grade],
+        sizeStyle[size],
+      )}
+    >
+      <span>{grade?.replaceAll("_", " ")}</span>
+      <span>{count}장</span>
+    </div>
+  );
+}
+// 사용법: <GradeMyCard grade="COMMON" count={20} size="S" />
