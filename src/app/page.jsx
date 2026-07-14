74905c1e7287dcb34d87b4e47737d6e1f2974b49
cari <aa34500388@gmail.com>
2026-07-14 13:37:13 +0900

    fix: 페이지 초기상태 복구

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,5 @@
-import PrimaryButton from "@/components/common/ButtonPrimary";
 import React from "react";
 
 export default function Home() {
-  return (
-    <div>
-      Home
-      <PrimaryButton variant="thick" size="L">
-        포토카드 구매하기
-      </PrimaryButton>
-      <PrimaryButton variant="thin" size="L">
-        포토카드 교환하기
-      </PrimaryButton>
-      <PrimaryButton variant="thin" size="L" disabled>
-        포토카드 교환하기
-      </PrimaryButton>
-    </div>
-  );
+  return <div>Home</div>;
 }
