86c9fc0c60bcc8d34420fe0c2f574427d940feea
apple <apple@appleui-MacBookAir.local>
2026-07-14 09:10:35 +0900

    feat: Chip 컴포넌트 작업 완료

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-export default function Chip({ type }) {
-  return type === "판매중" ? (
-    <strong className="text-[10px] font-normal text-white bg-black/50 rounded-xs px-2 py-1">
-      판매중
-    </strong>
-  ) : (
-    <strong className="text-[10px] font-normal text-[#EFFF04] bg-black/50 rounded-xs px-2.5 py-1">
-      교환 제시 대기 중
-    </strong>
+export default function Chip({ type="판매" }) {
+
+
+  return (
+    <>
+      {type === "판매" && (
+        <strong className="text-noto-10-regular font-normal text-white bg-black/50 rounded-xs px-2 py-1 tablet:text-noto-14-regular pc:text-noto-16-regular">
+          판매 중
+        </strong>
+      )}
+      {type === "교환" && (
+        <strong className="text-noto-10-regular font-normal text-[#EFFF04] bg-black/50 rounded-xs px-2.5 py-1 tablet:text-noto-14-regular pc:text-noto-16-regular">
+          교환 제시 대기 중
+        </strong>
+      )}
+    </>
   );
 }
