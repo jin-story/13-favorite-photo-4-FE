7ab5b8633524ab55982ad48117b4d0bfd01bed23
apple <apple@appleui-MacBookAir.local>
2026-07-14 11:50:22 +0900

    feat: Grade 컴포넌트 반응형 및 폰트 적용 완료

--- src/components/common/Grade.jsx
+++ src/components/common/Grade.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Grade({ type = "card", grade = "COMMON" }) {
+  if (type !== "card" && type !== "detail") return null;
   const upperGrade = grade.toUpperCase();
   const gradeStyles = {
     COMMON: "text-[#EFFF04]",
@@ -12,14 +13,14 @@ export default function Grade({ type = "card", grade = "COMMON" }) {
     <>
       {type === "card" && (
         <span
-          className={`text-[10px] font-light ${gradeStyles[upperGrade]} text-center`}
+          className={`text-noto-10-light ${gradeStyles[upperGrade]} text-center tablet:text-noto-16-light`}
         >
           {upperGrade}
         </span>
       )}
       {type === "detail" && (
         <span
-          className={` ${gradeStyles[upperGrade]} text-lg font-bold text-center`}
+          className={` ${gradeStyles[upperGrade]} text-noto-18-bold text-center tablet:text-noto-24-bold`}
         >
           {upperGrade}
         </span>
