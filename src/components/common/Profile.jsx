63a6792477819484290b03d723731b70bc8fbe82
apple <apple@appleui-MacBookAir.local>
2026-07-15 09:00:01 +0900

    feat: Profile 컴포넌트 작업 완료

---
+++ src/components/common/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ProfileMessage from "./ProfileMessage";
+import Link from "next/link";
+import clsx from "clsx";
+
+export default function Profile({ nickname, point, onLogout, className }) {
+  return (
+    <div
+      className={clsx(
+        "bg-gray-500 w-[260px] flex flex-col h-dvh tablet:h-auto",
+        className,
+      )}
+    >
+      <ProfileMessage nickname={nickname} point={point} />
+      <div className="flex flex-col flex-1">
+        <div className="text-noto-14-bold text-white flex flex-col gap-[15px] px-5 py-5 flex-1">
+          <Link href={"/"}>마켓플레이스</Link>
+          <Link href={"/"}>마이갤러리</Link>
+          <Link href={"/"}>판매 중인 포토카드</Link>
+        </div>
+        <div className="pb-5 text-noto-14-regular text-gray-400 px-5 tablet:hidden">
+          <button type="button" onClick={onLogout}>
+            로그아웃
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
