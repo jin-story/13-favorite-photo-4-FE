d664f6a5524d3e1289653f1559b48478ca91a213
apple <apple@appleui-MacBookAir.local>
2026-07-15 09:16:12 +0900

    chore: 주석 추가

--- src/components/common/Profile.jsx
+++ src/components/common/Profile.jsx
@@ -14,9 +14,9 @@ export default function Profile({ nickname, point, onLogout, className }) {
       <ProfileMessage nickname={nickname} point={point} />
       <div className="flex flex-col flex-1">
         <div className="text-noto-14-bold text-white flex flex-col gap-[15px] px-5 py-5 flex-1">
-          <Link href={"/"}>마켓플레이스</Link>
-          <Link href={"/"}>마이갤러리</Link>
-          <Link href={"/"}>판매 중인 포토카드</Link>
+          <Link href={"/"}>마켓플레이스</Link> {/*추후 주소 등록*/}
+          <Link href={"/"}>마이갤러리</Link> {/*추후 주소 등록*/}
+          <Link href={"/"}>판매 중인 포토카드</Link> {/*추후 주소 등록*/}
         </div>
         <div className="pb-5 text-noto-14-regular text-gray-400 px-5 tablet:hidden">
           <button type="button" onClick={onLogout}>
