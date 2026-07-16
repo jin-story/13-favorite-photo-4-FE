c53374b360e6eb47b0191e73757124567f725ee3
cari <aa34500388@gmail.com>
2026-07-16 16:54:42 +0900

    fix: page 초기셋팅 복구

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,5 @@
-import Gnb from "@/components/common/Gnb";
 import React from "react";
 
 export default function Home() {
-  const isLoggedIn = true; // false로 바꾸면 로그아웃 상태
-  const user = {
-    nickname: "유디",
-    point: 1540,
-  };
-  return (
-    <div>
-      <Gnb isLoggedIn={isLoggedIn} user={user} />
-    </div>
-  );
+  return <div>Home</div>;
 }
