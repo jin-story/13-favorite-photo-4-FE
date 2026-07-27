fc50a735907964a411271ae3b48500a04744f768
apple <apple@appleui-MacBookAir.local>
2026-07-27 13:27:44 +0900

    chore: 주석 삭제

--- src/lib/services/photoCardService.js
+++ src/lib/services/photoCardService.js
@@ -6,7 +6,6 @@ export const photoCardService = {
     const data = await tokenFetch("/photo-cards", {
       method: "POST",
       headers: {
-        // FormData를 사용할 때는 브라우저가 boundary를 자동으로 설정해야 하므로 Content-Type을 지워줍니다.
         "Content-Type": undefined,
       },
       body: formData,
