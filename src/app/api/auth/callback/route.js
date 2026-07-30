aab58b261204e30cd09c34df722c895db1811436
lucy <lucy.email@example.com>
2026-07-30 11:36:59 +0900

    fix: google Oauth

---
+++ src/app/api/auth/callback/route.js
@@ -0,0 +1,27 @@
+import { jwtDecode } from "jwt-decode";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  const token = request.nextUrl.searchParams.get("token");
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/error", request.url));
+  }
+
+  const { exp } = jwtDecode(token);
+  const maxAge = Math.max(0, exp - Math.floor(Date.now() / 1000));
+
+  const response = NextResponse.redirect(
+    new URL("/market-posting", request.url),
+  );
+
+  response.cookies.set("accessToken", token, {
+    path: "/",
+    maxAge,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  return response;
+}
